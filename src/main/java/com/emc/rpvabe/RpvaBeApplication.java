3438565caac8de4e9038972c7bd238f066feaf29
Nabil <Mina.Nabil@emc.com>
2019-03-04 11:30:17 +0200

    First Commit

---
+++ src/main/java/com/emc/rpvabe/RpvaBeApplication.java
@@ -0,0 +1,13 @@
+package com.emc.rpvabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpvaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpvaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emc/rpvabe/RpvaBeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.emc.rpvabe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RpvaBeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
