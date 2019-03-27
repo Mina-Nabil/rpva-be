2ccb90a34cfb780030405a368283871ed69b4c09
Assem <Mohamed.Assem@emc.com>
2019-03-27 15:50:25 +0200

    Testing assem:)

---
+++ src/main/java/com/emc/rpvabe/rpenv/Splitter.java
@@ -0,0 +1,13 @@
+
+package com.emc.rpvabe.rpenv;
+
+public class Splitter {
+  public String name;
+  public String type;
+  public String status;
+  public int attachedClusters;
+  public String attachedVolumes;
+  public String version;
+
+
+}
